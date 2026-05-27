16fae4e65acc369c23510d947b7843825449bb4a
Hendrik Jansen <hendrikjansen@posteo.net>
2026-05-27 14:24:34 +0200

    use faster TSP approximation

--- accessibility_equity/heuristics.py
+++ accessibility_equity/heuristics.py
@@ -1,6 +1,7 @@
 from typing import Dict
 from typing import Union
 from networkx.algorithms.approximation import traveling_salesman_problem as tsp
+from networkx.algorithms.approximation import simulated_annealing_tsp
 from accessibility_equity.wrappers.gym_wrapper import TransportGymWrapper
 from accessibility_equity.wrappers.dqn_wrapper import DQNTransportWrapper
 
@@ -15,7 +16,7 @@ class TSPVehicleAgent:
             self.env_type = 'DQN'
         else:
             raise TypeError(f"TSPVehicleAgent needs environment of type TransportGymWrapper or DQNTransportWrapper, not {type(env)}.")
-        self.route = tsp(self.env.env.network)[:-1]
+        self.route = tsp(self.env.env.network, method=simulated_annealing_tsp, init_cycle='greedy')[:-1]
         self.route_dict = {
             self.route[i]: self.route[(i + 1) % len(self.route)]
             for i in range(len(self.route))
@@ -25,7 +26,6 @@ class TSPVehicleAgent:
         if self.env_type == 'Gym':
             return obs["step_type"]
         if self.env_type == 'DQN':
-            print(type(obs))
             if type(obs) is dict:
                 return obs['features'][0]
             return obs[0]
